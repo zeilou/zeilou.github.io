d2fe7ea0cb4633fcd88c37595ed28622e237213f
zeilou <zeilou>
2021-02-23 18:13:23 +0800

    latest

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
-cd /root/zeilou.io/ && python3 main.py
-cd /root/zeilou.io/docs && cp -rf .vuepress/dist/* . && git pull origin master && git add -A && git commit -m "latest" && git push origin master
+cd /root/zeilou.io/ && python3 main.py && npm run build
+cd /root/zeilou.io/docs && cp -rf /root/zeilou.io/docs/.vuepress/dist/* /root/zeilou.io/docs/ && git pull origin master && git add -A && git commit -m "latest" && git push origin master
