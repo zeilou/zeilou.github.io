3bbe0aa0249c368602b128e36e1ef6bd0dc65858
zeilou <zeilou>
2021-02-23 16:15:24 +0800

    updated

--- submit.py
+++ submit.py
@@ -1,5 +1,6 @@
 import subprocess
 subprocess.call(["git", "pull"])
+#subprocess.call(["cp", "-rf",".vuepress/dist/*","."])
 subprocess.call(["git", "add", "-A"])
 subprocess.call(["git", "commit","-m","updated"])
 subprocess.call(["git", "push","origin","master"])
